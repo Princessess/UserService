a5ab00ea1120ad911dfe66ac24f2c7b14d108904
Caglayn <caglayankaya@hotmail.com.tr>
2022-11-27 13:26:24 +0300

    first commit

---
+++ src/main/java/com/c8n/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.c8n.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/c8n/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c8n.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
